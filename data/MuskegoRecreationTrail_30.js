84d20e0fda532a52db24c1d7c05da8297291f3dd
mappingmilwaukee <60715235+mappingmilwaukee@users.noreply.github.com>
2020-03-26 01:26:40 -0500

    Delete MuskegoRecreationTrail_30.js

--- data/MuskegoRecreationTrail_30.js
+++
@@ -1,9 +0,0 @@
-var json_MuskegoRecreationTrail_30 = {
-"type": "FeatureCollection",
-"name": "MuskegoRecreationTrail_30",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "trail name": "Muskego Recreation Trail", "lane type": "off-street paved", "notes": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -88.068467238286914, 42.902108232260133 ], [ -88.068571667467737, 42.902108232260133 ], [ -88.068789476901983, 42.902126809915366 ], [ -88.074786695571476, 42.902048128042928 ] ] ] } },
-{ "type": "Feature", "properties": { "trail name": "Muskego Rec Trail continues", "lane type": "off-street paved", "notes": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -88.07316632408218, 42.903070265985534 ], [ -88.074787282211261, 42.902045085559436 ], [ -88.073145663536565, 42.900988227263937 ] ] ] } }
-]
-}
