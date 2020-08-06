5a595383f79792292baffc698e825fe40b5ad406
mappingmilwaukee <60715235+mappingmilwaukee@users.noreply.github.com>
2020-08-05 19:25:24 -0500

    Add files via upload

---
+++ data/OzaukeeInterurbanTrail_26.js
@@ -0,0 +1,9 @@
+var json_OzaukeeInterurbanTrail_26 = {
+"type": "FeatureCollection",
+"name": "OzaukeeInterurbanTrail_26",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "trail name": "Ozaukee Interurban Trail", "lane type": "off-street paved", "notes": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -87.973571877867215, 43.19254651315984 ], [ -87.978445958455026, 43.203859526080805 ] ] ] } },
+{ "type": "Feature", "properties": { "trail name": "Ozaukee Interurban Trail continues", "lane type": "off-street paved", "notes": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -87.979346246807467, 43.202100441817187 ], [ -87.97843805213941, 43.203869131232523 ], [ -87.976194853244692, 43.202887144948122 ] ] ] } }
+]
+}
