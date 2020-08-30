9a3157949005efb52b7232c0b62a01fbe1e0d753
mappingmilwaukee <60715235+mappingmilwaukee@users.noreply.github.com>
2020-08-30 15:44:19 -0500

    Delete OLTBeerlineExtension_9.js

--- data/OLTBeerlineExtension_9.js
+++
@@ -1,8 +0,0 @@
-var json_OLTBeerlineExtension_9 = {
-"type": "FeatureCollection",
-"name": "OLTBeerlineExtension_9",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "trail name": "Oak Leaf Trail", "trail line": "Beerline B Extension", "lane type": "off-street paved", "notes": "coming TBA; https:\/\/urbanmilwaukee.com\/2020\/02\/24\/transportation-city-plans-4-pedestrian-bicycling-projects\/" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -87.914165167928275, 43.089645602818408 ], [ -87.917231213712014, 43.092895523377656 ], [ -87.918914080946692, 43.094487552755261 ], [ -87.919917434275135, 43.09510800012653 ], [ -87.920999826377852, 43.095715615619824 ], [ -87.923333665739207, 43.096667908511868 ], [ -87.927470028257375, 43.097936006190046 ], [ -87.936877685445893, 43.100931357388717 ] ] ] } }
-]
-}
