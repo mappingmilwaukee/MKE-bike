791440a6b83332ca85518cf4416d3b6e4842f2eb
mappingmilwaukee <60715235+mappingmilwaukee@users.noreply.github.com>
2020-09-07 17:25:15 -0500

    Add files via upload

---
+++ data/OzaukeeInterurbanTrail_27.js
@@ -0,0 +1,9 @@
+var json_OzaukeeInterurbanTrail_27 = {
+"type": "FeatureCollection",
+"name": "OzaukeeInterurbanTrail_27",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "trail name": "<a target=\"blank\" href=\"https:\/\/www.co.ozaukee.wi.us\/553\/Interurban-Trail\">Ozaukee Interurban Trail<\/a>", "lane type": "<a target=\"blank\" href=\"https:\/\/city.milwaukee.gov\/mpw\/infrastructure\/MilwaukeebyBike\/Bike-Facilities\/Trails\">off-street paved trail<\/a>", "notes": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -87.973571877867215, 43.19254651315984 ], [ -87.978445958455026, 43.203859526080805 ] ] ] } },
+{ "type": "Feature", "properties": { "trail name": "<a target=\"blank\" href=\"https:\/\/www.co.ozaukee.wi.us\/553\/Interurban-Trail\">Ozaukee Interurban Trail<\/a>", "lane type": null, "notes": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -87.979346246807467, 43.202100441817187 ], [ -87.97843805213941, 43.203869131232523 ], [ -87.976194853244692, 43.202887144948122 ] ] ] } }
+]
+}
