a9646593b3aba27bce6a22b38bd5f94e683f48ac
mappingmilwaukee <60715235+mappingmilwaukee@users.noreply.github.com>
2020-09-12 22:37:15 -0500

    Add files via upload

---
+++ data/ALERTS_23.js
@@ -0,0 +1,21 @@
+var json_ALERTS_23 = {
+"type": "FeatureCollection",
+"name": "ALERTS_23",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 - Chicago westbound lane blocked w\/ construction" }, "geometry": { "type": "Point", "coordinates": [ -87.906238699717164, 43.032770005167528 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 S2nd\/Plankinton bridge open some days, closed others; project supposed to wrap June 2020 re: DPW" }, "geometry": { "type": "Point", "coordinates": [ -87.912437052934209, 43.032611765472666 ] } },
+{ "type": "Feature", "properties": { "ALERT": "<b>DON'T RISK THE RR TRACKS! FOLLOW THE BIKE PATH ONTO THE DUMB SHARED SIDEWALK!<\/b>" }, "geometry": { "type": "Point", "coordinates": [ -87.906522516030805, 43.023198030293216 ] } },
+{ "type": "Feature", "properties": { "ALERT": "<b>DON'T RISK THE RR TRACKS! FOLLOW THE BIKE PATH ONTO THE DUMB SHARED SIDEWALK!<\/b>" }, "geometry": { "type": "Point", "coordinates": [ -87.907966700685336, 43.020955696013665 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 this intersection is a mess from construction, avoid - however Cleveland east of here is now open & smooth" }, "geometry": { "type": "Point", "coordinates": [ -87.93835232788139, 42.995726070342926 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 - concrete barriers & construction cones" }, "geometry": { "type": "Point", "coordinates": [ -87.9118627097493, 43.041856379419265 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 - S2 northbound lanes blocked" }, "geometry": { "type": "Point", "coordinates": [ -87.912536696859959, 43.029999738710579 ] } },
+{ "type": "Feature", "properties": { "ALERT": "very busy very narrow intersection - slow down!" }, "geometry": { "type": "Point", "coordinates": [ -87.96044551398586, 43.024761472104679 ] } },
+{ "type": "Feature", "properties": { "ALERT": "mind the 3\" gap at the curb cut!" }, "geometry": { "type": "Point", "coordinates": [ -87.904816420424254, 43.035272592686461 ] } },
+{ "type": "Feature", "properties": { "ALERT": "no westbound bike lane on St Paul until 2nd St - don't ride parallel to streetcar tracks!" }, "geometry": { "type": "Point", "coordinates": [ -87.909361363686244, 43.03497431909328 ] } },
+{ "type": "Feature", "properties": { "ALERT": "sinkhole on OLT Zip Line b\/t Silver Spring and Sidney Pl - take the detour onto Sidney Pl & Marne Ave or just life your bike around the sinkhole barriers" }, "geometry": { "type": "Point", "coordinates": [ -87.940673821795585, 43.125157568887708 ] } },
+{ "type": "Feature", "properties": { "ALERT": "watch out for two patches of bad loose red gravel here" }, "geometry": { "type": "Point", "coordinates": [ -87.935850217158972, 42.989627475897137 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Mason westbound Milwaukee-Bway blocked for Colby-Abbott bldg reconstruction" }, "geometry": { "type": "Point", "coordinates": [ -87.907020259777255, 43.040040983978649 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Jefferson southbound Mich-Clybourn blocked - tons of construction" }, "geometry": { "type": "Point", "coordinates": [ -87.905131053523576, 43.037032418471291 ] } }
+]
+}
