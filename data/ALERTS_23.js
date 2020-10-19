c9d91cfce34502fe08c2ad0f2d14fdcee42e536e
mappingmilwaukee <60715235+mappingmilwaukee@users.noreply.github.com>
2020-10-19 18:50:03 -0500

    Add files via upload

---
+++ data/ALERTS_23.js
@@ -0,0 +1,31 @@
+var json_ALERTS_23 = {
+"type": "FeatureCollection",
+"name": "ALERTS_23",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 - Chicago westbound lane blocked w\/ construction" }, "geometry": { "type": "Point", "coordinates": [ -87.906238699717164, 43.032770005167528 ] } },
+{ "type": "Feature", "properties": { "ALERT": "<b>DON'T RISK THE RR TRACKS! Especially with skinny tires. FOLLOW THE BIKE PATH ONTO THE DUMB SHARED SIDEWALK!<\/b>" }, "geometry": { "type": "Point", "coordinates": [ -87.906522516030805, 43.023198030293216 ] } },
+{ "type": "Feature", "properties": { "ALERT": "<b>DON'T RISK THE RR TRACKS! Especially with skinny tires. FOLLOW THE BIKE PATH ONTO THE DUMB SHARED SIDEWALK!<\/b>" }, "geometry": { "type": "Point", "coordinates": [ -87.907966700685336, 43.020955696013665 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 - bike lane obstruction: concrete barriers & construction cones" }, "geometry": { "type": "Point", "coordinates": [ -87.9118627097493, 43.041856379419265 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 - S2 northbound lanes blocked" }, "geometry": { "type": "Point", "coordinates": [ -87.912549397395139, 43.029947911928858 ] } },
+{ "type": "Feature", "properties": { "ALERT": "very busy very narrow intersection - slow down!" }, "geometry": { "type": "Point", "coordinates": [ -87.96044551398586, 43.024761472104679 ] } },
+{ "type": "Feature", "properties": { "ALERT": "no westbound bike lane on St Paul until 2nd St - don't ride parallel to streetcar tracks!" }, "geometry": { "type": "Point", "coordinates": [ -87.909039431820673, 43.03501976622978 ] } },
+{ "type": "Feature", "properties": { "ALERT": "sinkhole on OLT Zip Line b\/t Silver Spring and Sidney Pl - take the detour onto Sidney Pl & Marne Ave or just life your bike around the sinkhole barriers" }, "geometry": { "type": "Point", "coordinates": [ -87.940673821795585, 43.125157568887708 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Mason westbound Milwaukee-Bway blocked for Colby-Abbott bldg reconstruction" }, "geometry": { "type": "Point", "coordinates": [ -87.907020259777255, 43.040040983978649 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Jefferson southbound AND northbound Mich-Clybourn closed for construction" }, "geometry": { "type": "Point", "coordinates": [ -87.905131053523576, 43.037032418471291 ] } },
+{ "type": "Feature", "properties": { "ALERT": "high volume of traffic & high speeds on blind hill & corner" }, "geometry": { "type": "Point", "coordinates": [ -87.884174569293577, 42.989519431297168 ] } },
+{ "type": "Feature", "properties": { "ALERT": "a" }, "geometry": { "type": "Point", "coordinates": [ -87.871976244942644, 43.074877055640314 ] } },
+{ "type": "Feature", "properties": { "ALERT": "confidently enter the circle AND SIGNAL - OR take the wider sidewalks around the edge" }, "geometry": { "type": "Point", "coordinates": [ -87.917849709053272, 43.027147471581138 ] } },
+{ "type": "Feature", "properties": { "ALERT": "bunny hop if you can - BAD 2\" lip on the new trailhead - report to the City" }, "geometry": { "type": "Point", "coordinates": [ -87.896279024665702, 43.042554070467752 ] } },
+{ "type": "Feature", "properties": { "ALERT": "streetcar tracks start here - take the bike lane, NOT the tracks! AVOID with skinny tires!" }, "geometry": { "type": "Point", "coordinates": [ -87.900268596263629, 43.036678208324034 ] } },
+{ "type": "Feature", "properties": { "ALERT": "99% of the time someone will stop\/park in the Farwell bike lane betwen Brady & Ogden - CALL 414-286-4186 TO REPORT! You will need rough address; license plate photos are helpful" }, "geometry": { "type": "Point", "coordinates": [ -87.894178812119591, 43.051100680423467 ] } },
+{ "type": "Feature", "properties": { "ALERT": "new park path ends here but no curb cut so dismount and use the bike lane - the sidewalk is covered in glass, debris, etc." }, "geometry": { "type": "Point", "coordinates": [ -87.933829683578153, 42.993995836481425 ] } },
+{ "type": "Feature", "properties": { "ALERT": "new park path ends here but no curb cut - use the old one directly west" }, "geometry": { "type": "Point", "coordinates": [ -87.936448344287825, 42.995770386177412 ] } },
+{ "type": "Feature", "properties": { "ALERT": "STEPS - paths in north end of park have not been redone - lots of stairs\/portages" }, "geometry": { "type": "Point", "coordinates": [ -87.934661657768487, 42.998550856502433 ] } },
+{ "type": "Feature", "properties": { "ALERT": "helpful little bridge but debris & old RR tracks at south end" }, "geometry": { "type": "Point", "coordinates": [ -87.952970400507297, 42.995156883033808 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Planktinton northbound here lane blocked with orange barrels because of Reasons (no apparent reason)" }, "geometry": { "type": "Point", "coordinates": [ -87.911669508075164, 43.033828963150775 ] } },
+{ "type": "Feature", "properties": { "ALERT": "if you're going west on Kilbourn downhill with a green light at Water, do not bomb the downhill b\/c drivers turning left from Kilbourn to go north on Water will not see you!!!!" }, "geometry": { "type": "Point", "coordinates": [ -87.91015449873845, 43.042432120010517 ] } },
+{ "type": "Feature", "properties": { "ALERT": "S1 southbound bike lane blocked\/full of potholes & debris from Michels construction site" }, "geometry": { "type": "Point", "coordinates": [ -87.911273993087562, 43.007683785327359 ] } },
+{ "type": "Feature", "properties": { "ALERT": "Sept 2020 - Broadway lanes b\/t Mich-Clybourn blocked by hotel construction" }, "geometry": { "type": "Point", "coordinates": [ -87.907665665456477, 43.036876835029609 ] } }
+]
+}
